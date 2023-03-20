ed5ac4486005f76f8eb6af6549a33f789c6de24d
mark meiszterics <meisztericsmark333@gmail.com>
2023-03-20 21:32:38 +0100

    added new files

---
+++ src/components/Login.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="loginBox">
+      <h1>Belépés</h1>
+      <form method="post">
+        <div class="textField">
+          <input type="text" required>
+          <label>Felhasználónév</label>
+        </div>
+        <div class="textField">
+          <input type="password" required>
+          <label>Jelszó</label>
+        </div>
+        <input type="submit" value="Belépés" >
+        <br><br>
+      </form>
+    </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+*{
+    font-family: 'Montserrat', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.loginBox{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 350px;
+    background: white;
+    border-radius: 10px;
+}
+.loginBox form{ 
+    padding: 0 40px;
+    box-sizing: border-box;
+}
+.loginBox h1{
+    text-align: center;
+    padding: 20px 0;
+    border-bottom: 1px solid black;
+    color: black;
+}
+form .textField{
+    position: relative;
+    border-bottom: 2px solid black;
+    margin: 30px 0;
+}
+.textField input{
+    width: 100%;
+    padding: 0 5px;
+    height: 40px;
+    font-size: 16px;
+    border: none;
+    background: none;
+    outline: none;
+}
+.textField label{
+    position: absolute;
+    top: 50%;
+    left: 5px;
+    color: black;
+    transform: translateY(-50%);
+    font-size: 16px;
+    pointer-events: none;
+    transition: .5s;
+}
+.textField span::before{
+    width: 0%;
+    height: 2px;
+    top: 40px;
+    left: 0;
+    background: black;
+    transition: .2s;
+    position: absolute;
+}
+.textField input:focus ~ label,
+.textField input:valid ~ label{
+    top: -6px;
+    color: black;
+}
+.textField input:focus ~ span::before,
+.textField input:valid ~ span::before{
+    width: 100%;
+}
+.pass{
+    margin: -5px 0 20px 5px;
+    color: #a6a6a6;
+    cursor: pointer;
+}
+input[type="submit"]{
+    width: 100%;
+    height: 50px;
+    font-size: 20px;
+    color: #e9f4fb;
+    border: 5px solid;
+    border-radius: 25px;
+    background: #800080;
+}
+</style>
