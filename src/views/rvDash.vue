5ce98ead6c37834a8f5573892236f0784e0045d4
mark meiszterics <meisztericsmark333@gmail.com>
2023-03-23 15:42:04 +0100

    Designed B1 and B2

--- src/views/rvDash.vue
+++ src/views/rvDash.vue
@@ -1,29 +1,59 @@
 <template>
-    <div id="content">
-        <label>Név</label>
-        <input type="text" id="rvNameAdd"><br>
-
-        <label>Ár</label>
-        <input type="number" id="rvPriceAdd"><br>
-
-        <label>Darab</label>
-        <input type="number" id="rvQuantityAdd"><br><br>
-
-        <button type="submit" id="rvButtonAdd">Hozzáadás</button>
+    <div class="wrapper">
+        <div class="item2" id="b1">
+        <h1>Új hozzáadása</h1>
+        <table>
+            <tr>
+                <th>Név</th>
+                <th>Ár</th>
+                <th>Darab</th>
+            </tr>
+            <tr>
+                <td><input type="text" id="rvNameAdd"></td>
+                <td><input type="number" minlength="1" id="rvPriceAdd"></td>
+                <td><input type="number" minlength="1" id="rvQuantityAdd"></td>
+                <td><button type="submit" id="rvButtonAdd" onsubmit="">Hozzáadás</button></td>
+            </tr>
+        </table> 
     </div>
+    <div class="item4" id="b2">
+        <h1>Ár módosítása</h1>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Új ár</th>
+            </tr>
+            <tr>
+                <td><input type="number" minlength="1" id="rvIdUpdate"></td>
+                <td><input type="number" minlength="1" id="rvPriceUpdate"></td>
+                <td><button type="submit" id="rvButtonUpdate" onsubmit="">Módosít</button></td>
+            </tr>
+        </table>
+    </div>
+</div>
 </template>
 
 <style scoped>
-#content{
-    border-color: grey;
-    padding: 10px;
-    color: black;
+#b1{
+    font-size: 18px;
 }
-input[type=submit]{
-    color: black;
-    font-size: 12px;
+#b2{
+    font-size: 18px;
+}
+table{
     border: none;
-    margin: 2px 4px;
-
+    color: black;
+    background-color: white;
+    padding: 5px 5px;
+}
+input[type="text"], input[type="number"]{
+    width: 200px;
+    height: 35px;
+}
+button{
+    color: white;
+    border: none;
+    background-color: purple;
+    padding: 10px 10px;
 }
 </style>
