b0f9815a66e0538a7485d23acafadda31dec285e
mark meiszterics <meisztericsmark333@gmail.com>
2023-03-23 23:22:41 +0100

    Designed componentAdder

--- src/components/componentAdder.vue
+++ src/components/componentAdder.vue
@@ -1,32 +1,63 @@
 <template>
     <div class="wrapper">
-        <div class="container">
-        <div class="item">Név</div>
-        <div class="item">Ár</div>
-        <div class="item">Darab</div>
-        <input class="item" type="number">
-        <input class="item" type="number">
-        <input class="item" type="number">
-    </div>
+        <table class="tablazat">
+            <tr>
+                <th>Név</th>
+                <th>Ár</th>
+                <th>Darab</th>
+            </tr>
+            <tr>
+                <td><input class="item" id="bNameValue" type="text"></td>
+                <td><input class="item" id="bPriceValue" type="number"></td>
+                <td><input class="item" id="bQuantityValue" type="number"></td>
+                <td><input type="submit" @click=""></td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+            </tr>
+            <!-- a "response" -t kell átírni erre -> Sikeres / Sikertelen adatbevitel, exception handling is kell mellé-->
+            <tr>
+                <td id="response" colspan="3" style="text-align: center; vertical-align: middle;"></td>
+            </tr>
+        </table>
     </div>
 </template>
 <style scoped>
-.container {
-    display: grid;
+.wrapper {
+    height: 90vh; /*90vh volt az original*/
+    background-color: white;
     color: black;
-    padding: 10px;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr;
+    font-size: 16px;
+}
+.tablazat{
+    padding-top: 15%;
+    padding-bottom: 15%;
+    padding-left: 5%;
+    padding-right: 5%;
+    width: 100%;
+    height: auto;
+    font-size: 18px;
+    font-style: oblique;
 }
 
+input[type="submit"]{
+    padding: 10px;
+    width: 100px;
+    background-color: purple;
+    color:white;
+    height: 40px;
+    font-size: 18px;
+}
+
+input[type="text"], input[type="number"]{
+    padding: 10px;
+    height: 40px;
+    font-size: 18px;
+
+}
 .item {
-    border: 1px solid black;
     width: 100%;
     text-align: center;
-}
-
-.wrapper {
-    height: 90vh;
-    background-color: white;
 }
 </style>
