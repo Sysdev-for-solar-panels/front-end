9e166c59bb7ab228ccc98869981322e9c581fb3d
mark meiszterics <meisztericsmark333@gmail.com>
2023-03-23 13:58:56 +0100

    asd

---
+++ src/views/rvDash.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="content">
+        <label>Név</label>
+        <input type="text" id="rvNameAdd"><br>
+
+        <label>Ár</label>
+        <input type="number" id="rvPriceAdd"><br>
+
+        <label>Darab</label>
+        <input type="number" id="rvQuantityAdd"><br><br>
+
+        <button type="submit" id="rvButtonAdd">Hozzáadás</button>
+    </div>
+</template>
+
+<style scoped>
+#content{
+    border-color: grey;
+    padding: 10px;
+    color: black;
+}
+input[type=submit]{
+    color: black;
+    font-size: 12px;
+    border: none;
+    margin: 2px 4px;
+
+}
+</style>
