f27f8e1435d3eb2aaf3f4dcdf1919f597f00a707
green1490 <zambokrisztian1490@gmail.com>
2023-03-25 16:50:49 +0100

    new module version and fix where layout brake

--- src/components/componentChanger.vue
+++ src/components/componentChanger.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="wrapper">
-    <h1>Ár módosítása</h1>
-    <table>
+    <table class="tablazat">
         <tr>
             <th>ID</th>
             <th>Új ár</th>
         </tr>
         <tr>
-            <td><input type="number" minlength="1" id="rvIdUpdate"></td>
-            <td><input type="number" minlength="1" id="rvPriceUpdate"></td>
+            <td><input class="item" type="number" minlength="1" id="rvIdUpdate"></td>
+            <td><input class="item" type="number" minlength="1" id="rvPriceUpdate"></td>
             <td><button type="submit" id="rvButtonUpdate" onsubmit="">Módosít</button></td>
         </tr>
     </table>
@@ -16,10 +15,44 @@
 </template>
 
 <style scoped>
+
+.tablazat{
+    padding-top: 15%;
+    padding-bottom: 15%;
+    padding-left: 5%;
+    padding-right: 5%;
+    width: 100%;
+    height: auto;
+    font-size: 18px;
+    font-style: oblique;
+}
+
+button {
+    padding: 10px;
+    width: 100px;
+    background-color: purple;
+    color:white;
+    height: 40px;
+    font-size: 18px;
+}
+
+input[type="text"], input[type="number"]{
+    padding: 10px;
+    height: 40px;
+    font-size: 18px;
+
+}
+
 .wrapper {
     height: 90vh; /*90vh volt az original*/
     background-color: rgb(255, 255,255, 0.8);
     color: black;
     font-size: 16px;
+}
+
+
+.item {
+    width: 100%;
+    text-align: center;
 }
 </style>
